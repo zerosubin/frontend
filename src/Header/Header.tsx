edc059214bf0dbc817f1832d9f4e9c83c8bf28c4
subin <psb2638@naver.com>
2023-07-17 16:06:26 +0900

    feat: router

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,17 +1,18 @@
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-
 
 const StyledHeader = styled.div`
   .modalHeader {
     position: absolute;
     width: 100%;
+
     display: flex;
-    top: 0;
     justify-content: space-around;
-    font-size: 10px;
-    color: white;
-    background-color: skyblue;
+    top: 8px;
+    gap: 126px;
+
+    font-size: 12px;
   }
 
   h2:hover{
@@ -19,12 +20,30 @@ const StyledHeader = styled.div`
   }
 `;
 
+const MentBox =  styled.div`
+  display: flex;
+`
+
+const Blue = styled.h2`
+  padding: 0 4px;
+  color: #0089B5;
+`
+
 const Header: React.FC = () => {
     return ReactDOM.createPortal(
       <StyledHeader>
         <div className="modalHeader">
-          <h2>우리동네 해결사</h2>
-          <h2>로그인</h2>
+
+          <Link to="/" style={{ textDecoration: "none", color: "#000"}}>
+            <MentBox>
+              <h2>우리동네</h2><Blue>해결사</Blue>
+            </MentBox>
+          </Link>
+
+          <Link to="/login" style={{ textDecoration: "none", color: "#000"}}>
+            <h2>로그인</h2>
+          </Link>
+
         </div>
       </StyledHeader>,
       document.getElementById('modal-root')!
---
+++ src/Pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from 'styled-components';
+
+export default function LoginPage() {
+  return (
+    <Container>LoginPage</Container>
+  )
+}
+
+const Container = styled.section`
+  heigth: 100vh;
+  margin: 60px 22px;
+
+  background-color: red; 
+`
