95f63ca46d7679c6c1363fd2f67302599ef9f67b
subin <psb2638@naver.com>
2023-07-27 18:33:48 +0900

    feat: ChattingProfilePage 레이아웃

--- src/Pages/ChattingProfilePage/ChattingProfilePage.tsx
+++ src/Pages/ChattingProfilePage/ChattingProfilePage.tsx
@@ -1,9 +1,19 @@
-import { SC } from './styled'
+import * as SC from './styled'
 
 export default function ChattingProfilePage() {
   return (
     <SC.Container>
-      <SC.Title>ChattingPage</SC.Title>
+      <SC.ImgBox>
+        <SC.Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg7XZ2Imq08ItmvrtqXI-ZRiWNlow7qIshDg&usqp=CAU"/>
+      </SC.ImgBox>
+      <SC.ProfileBox>
+        <SC.Name>스펀지밥</SC.Name>
+        <SC.Location>서초대로77길</SC.Location>
+        <SC.ReviewBOX>
+          <SC.Title>매너 등급</SC.Title>
+          <SC.LankScore>1</SC.LankScore>
+        </SC.ReviewBOX>
+      </SC.ProfileBox>
     </SC.Container>
   )
 }
