deddadca0549813a356d85609bc6c8b243feb31e
joshyeom <jhyeom95@daum.net>
2023-08-04 15:46:15 +0900

    saveMarkers

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,34 +1,31 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react'
 import MapComponent from './MapComponent';
 import { SC } from './styled';
-import { instanceHeader } from '../API/axiosAPI';
 
 const MainPage: React.FC = () => {
-
-  const logintoken = sessionStorage.getItem('user')
-  const getUser = () => {
-    if(logintoken) {
-      try {
-        instanceHeader({
-          url: 'users/',
-          method: 'get',
-        })
-      } catch (error: any) {
-        console.log(error)
-      }
-    }
-  }
+  const [mapCenter, setMapCenter] = useState<{lat: number, lon: number} | null>(null);
 
   useEffect(() => {
-    getUser()
-  }, [])
-  
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(
+        (postion) => {
+          const lat = postion.coords.latitude;
+          const lon = postion.coords.longitude
+          setMapCenter({lat, lon})
+        }
+        )
+      }else{
+        alert('위치 정보를 불러올 수 없습니다.')
+      }
+    }, [])
 
-  return (
-    <SC.MapBox>
-      <MapComponent mapCenter={{ lat: 33.450701, lon: 126.570667 }} />
-    </SC.MapBox>
-  );
+    if(mapCenter){
+      return (
+        <SC.MapBox>
+          <MapComponent mapCenter={ mapCenter } />
+        </SC.MapBox>
+      );
+    }
 };
 
 export default MainPage;
